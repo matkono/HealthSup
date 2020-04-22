822eab829e3a1bfd2540fcd38ee8563e590d3437
Mateus Kono <mateuskono@gmail.com>
2020-04-22 17:55:33 -0300

    Add authorization to access api by swagger.

---
+++ src/Cardiompp.WebApi/Configurations/Swagger/ConfigureSwaggerOptions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Affiliation.WebApi.Configurations.Swagger
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    { 
+        public void Configure(SwaggerGenOptions options)
+        {
+            SetSecurity(options);
+
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "Cardiompp API", Version = "v1" });
+        }
+
+        private static void SetSecurity(SwaggerGenOptions options)
+        {
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization header using the Bearer scheme."
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
+                }
+            });
+        }
+    }
+}
