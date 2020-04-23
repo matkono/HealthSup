89c4f7868eacb91be9f46381b3a50be01189ca49
Mateus Kono <mateuskono@gmail.com>
2020-04-23 17:40:11 -0300

    Add service

---
+++ src/Cardiompp.Application/Services/DoctorService.cs
@@ -0,0 +1,14 @@
+﻿using Cardiompp.Application.DataContracts.v1.Responses.Doctor;
+using Cardiompp.Application.Services.Contracts;
+using System.Threading.Tasks;
+
+namespace Cardiompp.Application.Services
+{
+    public class DoctorService : IDoctorService
+    {
+        public Task<GetDoctorResponse> GetByAffiliationCodeAsync(string crm)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
