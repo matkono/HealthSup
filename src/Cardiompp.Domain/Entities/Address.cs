3ccf130297e413f1fb07fac5128f260222e7be0f
Mateus Kono <mateuskono@gmail.com>
2020-04-23 17:41:10 -0300

    Add entities

---
+++ src/Cardiompp.Domain/Entities/Address.cs
@@ -0,0 +1,24 @@
+﻿namespace Cardiompp.Domain.Entities
+{
+    public class Address
+    {
+        public Address(
+            int id, 
+            string region,
+            string cep
+        ) 
+        {
+            Id = id;
+            Region = region;
+            Cep = cep;
+        }
+
+        public Address() { }
+
+        public int Id { get; private set; }
+
+        public string Region { get; private set; }
+
+        public string Cep { get; private set; }
+    }
+}
