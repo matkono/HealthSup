658340c02a16bdb00537d74ff785579fb4831b8e
Mateus Kono <mateuskono@gmail.com>
2020-04-23 17:41:42 -0300

    Remove default classes

--- src/Cardiompp.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Cardiompp.Domain
-{
-    public class Class1
-    {
-    }
-}
--- src/Cardiompp.Infrastructure/Cardiompp.Infrastructure.CrossCutting/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Cardiompp.Infrastructure.CrossCutting
-{
-    public class Class1
-    {
-    }
-}
