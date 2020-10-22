87a3994cfb1336da2c90bc80ad0ed131f2988db1
Mateus Kono <mateuskono@gmail.com>
2020-10-22 12:16:49 -0300

    Remove unnecessary code

--- src/HealthSup.Domain/Services/Contracts/INodeDomainService.cs
+++ src/HealthSup.Domain/Services/Contracts/INodeDomainService.cs
@@ -4,12 +4,7 @@ using System.Threading.Tasks;
 namespace HealthSup.Domain.Services.Contracts
 {
     public interface INodeDomainService
-    {
-        Task<Node> GetInitialByDecisionTreeId
-        (
-            int decisionTreeId
-        );
-
+    { 
         Task<Node> ResolveByMedicalAppointment
         (
             MedicalAppointment medicalAppointment
--- src/HealthSup.Domain/Services/NodeDomainService.cs
+++ src/HealthSup.Domain/Services/NodeDomainService.cs
@@ -19,19 +19,6 @@ namespace HealthSup.Domain.Services
 
         private readonly IUnitOfWork _unitOfWork;
 
-        public async Task<Node> GetInitialByDecisionTreeId
-        (
-            int decisionTreeId
-        )
-        {
-            var initialNode = await _unitOfWork.NodeRepository.GetInitialByDecisionTreeId
-            (
-                decisionTreeId
-            );
-
-            return await LoadInitialNodeDetailsAsync(initialNode.Id, initialNode.NodeType.Id);
-        }
-
         public async Task<Node> ResolveByMedicalAppointment
         (
             MedicalAppointment medicalAppointment
@@ -49,28 +36,6 @@ namespace HealthSup.Domain.Services
             }
 
             return await LoadNodeDetails(node.Id, node.NodeType.Id);
-        }
-
-        private async Task<Node> LoadInitialNodeDetailsAsync
-        (
-            int nodeId,
-            int nodeTypeId
-        ) 
-        {
-            switch (nodeTypeId)
-            {
-                case (int)NodeTypeEnum.Action:
-                    var action = await _unitOfWork.ActionRepository.GetByNodeId(nodeId);
-                    return action;
-
-                case (int)NodeTypeEnum.Question:
-                    var question = await _unitOfWork.QuestionRepository.GetByNodeId(nodeId);
-                    question.SetPossibleAnswers(await _unitOfWork.PossibleAnswerRepository.ListByQuestionId(question.Id));
-                    return question;
-
-                default:
-                    throw new InvalidInitialNodeTypeException("NodeTypeId is invalid to be initial node.");
-            }
         }
 
         private async Task<Node> LoadNodeDetails
