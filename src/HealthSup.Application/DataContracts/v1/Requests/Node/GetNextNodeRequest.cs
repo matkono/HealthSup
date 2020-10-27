04de4002ade002ad2ba6592849935a42b0a9fd13
Mateus Kono <mateuskono@gmail.com>
2020-10-27 03:22:40 -0300

    Add new data contracts.

---
+++ src/HealthSup.Application/DataContracts/v1/Requests/Node/GetNextNodeRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthSup.Application.DataContracts.v1.Requests.Node
+{
+    public class GetNextNodeRequest
+    {
+        [Required(ErrorMessage = "MedicalAppointmentId is required to get next node.")]
+        public int MedicalAppointmentId { get; set; }
+
+        [Required(ErrorMessage = "DoctorId is required to get next node.")]
+        public int DoctorId { get; set; }
+
+        [Required(ErrorMessage = "QuestionId is required to get next node.")]
+        public int QuestionId { get; set; }
+
+        [Required(ErrorMessage = "SetPossibleAnswerId is required to get next node.")]
+        public int SetPossibleAnswerId { get; set; }
+
+        [Required(ErrorMessage = "Date is required to get next node.")]
+        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "PossibleAnswersId is required to get next node.")]
+        public List<int> PossibleAnswersId { get; set; }
+
+    }
+}
