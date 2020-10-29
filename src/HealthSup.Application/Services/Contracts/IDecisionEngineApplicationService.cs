d619d2293ebdd5dd7dbdac3b8de1b7d684a15bef
Mateus Kono <mateuskono@gmail.com>
2020-10-29 10:41:43 -0300

    Add application service for decision engine

---
+++ src/HealthSup.Application/Services/Contracts/IDecisionEngineApplicationService.cs
@@ -0,0 +1,14 @@
+﻿using HealthSup.Application.DataContracts.v1.Requests.Node;
+using HealthSup.Application.DataContracts.v1.Responses.Node;
+using System.Threading.Tasks;
+
+namespace HealthSup.Application.Services.Contracts
+{
+    public interface IDecisionEngineApplicationService
+    {
+        public Task<GetNextNodeReturn> GetNextNode
+        (
+            GetNextNodeRequest argument
+        );
+    }
+}
---
+++ src/HealthSup.Application/Services/DecisionEngineApplicationService.cs
@@ -0,0 +1,34 @@
+﻿using HealthSup.Application.DataContracts.v1.Requests.Node;
+using HealthSup.Application.DataContracts.v1.Responses.Node;
+using HealthSup.Application.Services.Contracts;
+using HealthSup.Application.Validators;
+using HealthSup.Domain.Repositories;
+using System.Threading.Tasks;
+
+namespace HealthSup.Application.Services
+{
+    public class DecisionEngineApplicationService : IDecisionEngineApplicationService
+    {
+        public DecisionEngineApplicationService
+        (
+            IUnitOfWork unitOfWork
+        )
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public async Task<GetNextNodeReturn> GetNextNode
+        (
+            GetNextNodeRequest argument
+        )
+        {
+            var validator = new GetNextNodeValidator(_unitOfWork);
+
+            var result = validator.Validate(argument);
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
