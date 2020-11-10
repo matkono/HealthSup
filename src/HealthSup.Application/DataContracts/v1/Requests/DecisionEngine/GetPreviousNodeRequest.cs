2a514706d36d3de0998c3fa54a239815ffb4fa44
Mateus Kono <mateuskono@gmail.com>
2020-11-10 10:13:28 -0300

    Add contracts

---
+++ src/HealthSup.Application/DataContracts/v1/Requests/DecisionEngine/GetPreviousNodeRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace HealthSup.Application.DataContracts.v1.Requests.DecisionEngine
+{
+    public class GetPreviousNodeRequest
+    {
+        public int MedicalAppointmentId { get; set; }
+
+        public int CurrentNodeId { get; set; }
+    }
+}
