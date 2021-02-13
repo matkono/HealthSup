9867d9fcac2993402fdca9e9cc177e7c300456da
Mateus Kono <mateuskono@gmail.com>
2021-02-13 09:34:11 -0300

    Change name argument

--- src/HealthSup.Application/DataContracts/v1/Requests/Disease/ListPagedRequest.cs
+++ src/HealthSup.Application/DataContracts/v1/Requests/Disease/ListPagedRequest.cs
@@ -1,6 +1,6 @@
 ﻿namespace HealthSup.Application.DataContracts.v1.Requests.Disease
 {
-    public class ListPagedDiseaseRequest
+    public class ListPagedRequest
     {
         public Pagination Pagination { get; set; }
     }
