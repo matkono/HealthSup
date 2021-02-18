de2c69bc4f1df1c48cb3da4aad8f733fccf687fc
Mateus Kono <mateuskono@gmail.com>
2021-02-18 20:40:31 -0300

    Remove unnecessary data contract

--- src/HealthSup.Application/DataContracts/v1/Requests/Patient/GetPatientByRegistrationRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace HealthSup.Application.DataContracts.v1.Requests.Patient
-{
-    public class GetPatientByRegistrationRequest
-    {
-        public string Registration { get; set; }
-    }
-}
