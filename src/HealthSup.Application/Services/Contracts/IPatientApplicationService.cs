34f46b2784a2696708a3c65edeba3cb3c874233a
Mateus Kono <mateuskono@gmail.com>
2021-03-06 13:47:02 -0300

    Fix list patient

--- src/HealthSup.Application/Services/Contracts/IPatientApplicationService.cs
+++ src/HealthSup.Application/Services/Contracts/IPatientApplicationService.cs
@@ -1,4 +1,4 @@
-﻿using HealthSup.Application.DataContracts.v1.Requests.Patient;
+﻿using HealthSup.Application.DataContracts.v1.Requests;
 using HealthSup.Application.DataContracts.v1.Responses.Patient;
 using System.Threading.Tasks;
 
@@ -8,7 +8,7 @@ namespace HealthSup.Application.Services.Contracts
     {
         public Task<ListPatientsPagedReturn> ListPaged
         (
-            ListPatientPagedRequest argument
+            Pagination pagination
         );
 
         public Task<GetPatientByRegistrationReturn> GetByRegistration
--- src/HealthSup.Application/Services/PatientApplicationService.cs
+++ src/HealthSup.Application/Services/PatientApplicationService.cs
@@ -1,8 +1,7 @@
-﻿using HealthSup.Application.DataContracts.v1.Requests.Patient;
+﻿using HealthSup.Application.DataContracts.v1.Requests;
 using HealthSup.Application.DataContracts.v1.Responses.Patient;
 using HealthSup.Application.Mappers;
 using HealthSup.Application.Services.Contracts;
-using HealthSup.Domain.Enums;
 using HealthSup.Domain.Repositories;
 using HealthSup.Infrastructure.CrossCutting.Constants;
 using System.Threading.Tasks;
@@ -21,10 +20,10 @@ namespace HealthSup.Application.Services
 
         private readonly IUnitOfWork _unitOfWork;
 
-        public async Task<ListPatientsPagedReturn> ListPaged(ListPatientPagedRequest argument)
+        public async Task<ListPatientsPagedReturn> ListPaged(Pagination pagination)
         {
-            var pageNumber = argument.Pagination.PageNumber;
-            var pageSize = argument.Pagination.PageSize > ApplicationConstants.MaxPageSize ? ApplicationConstants.MaxPageSize : argument.Pagination.PageSize;
+            var pageNumber = pagination.PageNumber;
+            var pageSize = pagination.PageSize > ApplicationConstants.MaxPageSize ? ApplicationConstants.MaxPageSize : pagination.PageSize;
 
             var patients = await _unitOfWork.PatientRepository.ListPaged(pageNumber, pageSize);
 
